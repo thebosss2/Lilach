4e15f3db0f83187812e42ba06b11abd3513fd387
galh9 <galh97@gmail.com>
2022-04-23 16:17:09 +0300

    improved design

--- Prototype-0.0.1/src/main/java/org/example/ProductController.java
+++ Prototype-0.0.1/src/main/java/org/example/ProductController.java
@@ -15,9 +15,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
 public class ProductController extends Controller{
+
+    @FXML
+    private Pane pane;
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -42,23 +46,6 @@ public class ProductController extends Controller{
     @FXML // fx:id="priceBeforeDiscount"
     private Text priceBeforeDiscount; // Value injected by FXMLLoader
 
-    @FXML
-    void addToCart(ActionEvent event) throws InterruptedException {
-
-        Executor executor = Executors.newSingleThreadExecutor();
-        executor.execute(() -> {
-            button.setStyle("-fx-background-color: #f0a5b3");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            button.setStyle("-fx-background-color: #FFC1CC");
-        }
-        //Check if you need to kill the thread.
-);
-
-    }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
@@ -67,6 +54,14 @@ public class ProductController extends Controller{
         assert image != null : "fx:id=\"image\" was not injected: check your FXML file 'Product.fxml'.";
         assert price != null : "fx:id=\"price\" was not injected: check your FXML file 'Product.fxml'.";
         assert priceBeforeDiscount != null : "fx:id=\"priceBeforeDiscount\" was not injected: check your FXML file 'Product.fxml'.";
+
+    }
+
+    @FXML
+    void addToCart(ActionEvent event) throws InterruptedException {
+
+        coolButtonClick((Button)event.getTarget());
+        //Check if you need to kill the thread.
 
     }
 
@@ -83,7 +78,8 @@ public class ProductController extends Controller{
             priceBeforeDiscount.setText("");
     }
 
-    public void goToProductView (MouseEvent event) {
+    public void goToProductView (MouseEvent event) throws InterruptedException {
+        clickOnProductEffect(event);
         ProductViewController productView = null;
         Controller controller = null;
         StoreSkeleton skeleton = this.getSkeleton();
@@ -91,4 +87,45 @@ public class ProductController extends Controller{
         productView = (ProductViewController) controller;
         productView.setProduct(this.product);
     }
+
+    private void coolButtonClick(Button button) throws InterruptedException{
+        Executor executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            button.setStyle("-fx-background-color: #f0a5b3");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            button.setStyle("-fx-background-color: #FFC1CC");
+        });
+    }
+
+    @FXML
+    void mouseOnProduct(MouseEvent event)  {
+        pane.setStyle("-fx-background-color: #c7ff96");
+    }
+
+    @FXML
+    void mouseOffProduct(MouseEvent event)  {
+        pane.setStyle("-fx-background-color: #ffffff");
+    }
+
+    @FXML
+    void clickOnProductEffect(MouseEvent event) throws InterruptedException {
+
+         pane.setStyle("-fx-background-color: #8fb66a");
+         try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        pane.setStyle("-fx-background-color: #ffffff");
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
