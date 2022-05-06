4430dec5065db340512667c9253d3634b0716722
Gal Halperin <galh97@gmail.com>
2022-05-06 12:06:04 +0300

    Order class created (in class diagram we forgot about pickup or delivery attribute, so we added it), in EditProduct we added the option to change an image and save it in DB.

---
+++ entities/src/main/java/org/entities/Order.java
@@ -0,0 +1,90 @@
+package org.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.*;
+
+enum PickupOrDelivery {PICKUP, DELIVERY}
+
+@Entity
+@Table(name = "orders")
+public class Order implements Serializable {      //Product class entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;         // id generated for each product
+    @ManyToOne
+    private Product[] products;
+
+    //TODO - need to make RegisteredUser Class
+    //private RegisteredUser orderedBy;
+
+    private int price;
+    private Date orderTime;
+    private Date DeliveryTime;
+    private PickupOrDelivery pickupOrDelivery;
+    private boolean isDelivered;
+
+
+    public Order(Product[] products,/* RegisteredUser orderedBy,*/
+                 int price, Date orderTime, Date deliveryTime, PickupOrDelivery pickupOrDelivery) {
+        this.products = products;
+        //this.orderedBy = orderedBy;
+        this.price = price;
+        this.orderTime = orderTime;
+        this.DeliveryTime = deliveryTime;
+        this.pickupOrDelivery = pickupOrDelivery;
+        this.isDelivered = false;
+    }
+
+    public Order() {
+
+    }
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(Product[] products) {
+        this.products = products;
+    }
+
+    /*public RegisteredUser getOrderedBy() {
+        return orderedBy;
+    }
+
+    public void setOrderedBy(RegisteredUser orderedBy) {
+        this.orderedBy = orderedBy;
+    }*/
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Date orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    public Date getDeliveryTime() {
+        return DeliveryTime;
+    }
+
+    public void setDeliveryTime(Date deliveryTime) {
+        DeliveryTime = deliveryTime;
+    }
+
+    public boolean isDelivered() {
+        return isDelivered;
+    }
+
+    public void setDelivered(boolean delivered) {
+        isDelivered = delivered;
+    }
+}
