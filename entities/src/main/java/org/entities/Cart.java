ba036f6df5a1174dca4d8b9a50edc60b97f0e23b
Gal Halperin <galh97@gmail.com>
2022-05-11 10:49:28 +0300

    Cart class added (Yahav & May implementation)

---
+++ entities/src/main/java/org/entities/Cart.java
@@ -0,0 +1,41 @@
+package org.entities;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Cart {
+
+    private List<Product> products;
+    private int totalCost;
+
+    public Cart() {
+        this.products = new LinkedList<Product>();
+        this.totalCost = 0;
+    }
+
+    public double getTotalCost() {
+        return this.totalCost;
+    }
+
+    public void setTotalCost(int newCost) {
+        this.totalCost = newCost;
+    }
+
+    public void removeProduct(int id) {
+        for (int i = 0; i < this.products.size(); i++) {
+            if (this.products.get(i).getId() == id) {
+                totalCost -= this.products.get(i).getPrice();
+                products.remove(i);
+                break;
+            }
+        }
+    }
+
+    public void insertProduct(Product product) {
+        this.products.add(product);
+        this.totalCost += product.getPrice();
+    }
+}
+
