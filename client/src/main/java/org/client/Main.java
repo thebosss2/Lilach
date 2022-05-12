e34a518eae411e84664e0e54008a884d5226afc7
tahel lazar <tahellazar@gmail.com>
2022-05-12 11:40:28 +0300

    create order gui

--- client/src/main/java/org/client/Main.java
+++ client/src/main/java/org/client/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         try {
 
-            App.main(args);     //Main class for Jar calls cliant main
+            App.main(args);     //Main class for Jar calls client main
         } catch (IOException e) {
             e.printStackTrace();
         }
