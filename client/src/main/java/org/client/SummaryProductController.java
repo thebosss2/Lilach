10d55de1a4a3ee79810ccee554b71ec36b2d09ed
May <may4her@gmail.com>
2022-05-14 18:50:49 +0300

    orders FXML's

---
+++ client/src/main/java/org/client/SummaryProductController.java
@@ -0,0 +1,38 @@
+package org.client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class SummaryProductController{
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Text description;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Text price;
+
+    @FXML
+    private Text product_name;
+
+    @FXML
+    void initialize() {
+        assert description != null : "fx:id=\"description\" was not injected: check your FXML file 'SummaryProduct.fxml'.";
+        assert image != null : "fx:id=\"image\" was not injected: check your FXML file 'SummaryProduct.fxml'.";
+        assert price != null : "fx:id=\"price\" was not injected: check your FXML file 'SummaryProduct.fxml'.";
+        assert product_name != null : "fx:id=\"product_name\" was not injected: check your FXML file 'SummaryProduct.fxml'.";
+
+    }
+
+}
