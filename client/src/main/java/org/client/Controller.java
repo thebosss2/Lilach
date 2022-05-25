31dea2b2d25afe4aea177d9d9123e887c23f37cb
IZ <itaize33@gmail.com>
2022-05-25 14:03:04 +0300

    update sendalert and pom for mail

--- client/src/main/java/org/client/Controller.java
+++ client/src/main/java/org/client/Controller.java
@@ -1,5 +1,6 @@
 package org.client;
 
+import javafx.application.Platform;
 import javafx.scene.control.*;
 
 import java.time.LocalDate;
@@ -75,5 +76,20 @@ public abstract class Controller {
                 return true;
             }
         } return false;
+    }
+    public static void sendAlert(String error, String title, Alert.AlertType type) {
+        Platform.runLater(() -> {
+            try{
+                Alert alert = new Alert(type);
+                alert.setTitle(title);
+                alert.setHeaderText(error);
+                //alert.setContentText("Are you sure?");
+                alert.showAndWait().get();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        });
+
+
     }
 }
